a69ed44533f15157f010a1685ab123f44e0feca7
abuhasanmahmud <mahmud.me420@gmail.com>
2023-08-18 11:15:27 +0600

    ss

--- api/index.js
+++ api/index.js
@@ -18,7 +18,7 @@ app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.get("/", (req, res) => {
+app.use("/", (req, res) => {
   res.send("app successfully running");
 });
 
